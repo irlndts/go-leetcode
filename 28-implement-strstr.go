a87f384018af249e0e68f3f96246908678a70b7e
irlndts <kleto4kin@gmail.com>
2020-01-13 21:46:15 +0300

    more

---
+++ 28-implement-strstr.go
@@ -0,0 +1,9 @@
+package leetcode
+
+import "strings"
+
+// https://leetcode.com/problems/implement-strstr/
+
+func strStr(haystack string, needle string) int {
+	return strings.Index(haystack, needle)
+}
