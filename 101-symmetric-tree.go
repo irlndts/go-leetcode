52974aeba0ba68f43029a547ce190b66b62e54eb
irlndts <kleto4kin@gmail.com>
2020-05-06 16:02:00 +0300

    Symmetric tree

---
+++ 101-symmetric-tree.go
@@ -0,0 +1,27 @@
+package leetcode
+
+// https://leetcode.com/problems/symmetric-tree/
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+
+	return symmetric(root.Left, root.Right)
+}
+
+func symmetric(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+
+	if left == nil || right == nil {
+		return false
+	}
+
+	if left.Val != right.Val {
+		return false
+	}
+
+	return symmetric(left.Right, right.Left) && symmetric(left.Left, right.Right)
+}
