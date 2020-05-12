5ce7cb9fb8c764cddc146596db80376a06d3a08c
irlndts <kleto4kin@gmail.com>
2020-05-12 17:50:20 +0300

    Valid Palindrome

---
+++ 125-valid-palindrome.go
@@ -0,0 +1,35 @@
+package leetcode
+
+import (
+	"strings"
+	"unicode"
+)
+
+// https://leetcode.com/problems/valid-palindrome/
+
+func isPalindrome(s string) bool {
+	if len(s) == 0 {
+		return true
+	}
+	s = strings.ToLower(s)
+
+	l, r := 0, len(s)-1
+	for l <= r {
+		if !unicode.IsDigit(rune(s[l])) && !unicode.IsLetter(rune(s[l])) {
+			l++
+			continue
+		}
+
+		if !unicode.IsDigit(rune(s[r])) && !unicode.IsLetter(rune(s[r])) {
+			r--
+			continue
+		}
+
+		if s[l] != s[r] {
+			return false
+		}
+		l++
+		r--
+	}
+	return true
+}
