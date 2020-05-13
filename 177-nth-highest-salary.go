6d6dfef9e40effcf57f00d990c55f873b23ecf8f
irlndts <kleto4kin@gmail.com>
2020-05-13 09:27:13 +0300

    Nth Highest Salary

---
+++ 177-nth-highest-salary.go
@@ -0,0 +1,15 @@
+package leetcode
+
+// https://leetcode.com/problems/nth-highest-salary/
+
+/*
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  declare i int;
+  set i = N - 1;
+  RETURN (
+      # Write your MySQL query statement below.
+      select Salary from Employee group by Salary order by Salary desc limit i, 1
+  );
+END
+*/
