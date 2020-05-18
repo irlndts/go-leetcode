0d7ad029b9be80f64fceef2fb83d450f14a9796d
irlndts <kleto4kin@gmail.com>
2020-05-18 12:58:01 +0300

    Best time to buy and sell stock

---
+++ 121-best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,23 @@
+package leetcode
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+func maxProfit(prices []int) int {
+	var result int
+	if len(prices) <= 1 {
+		return result
+	}
+
+	l, r := prices[0], 0
+	for _, p := range prices {
+		r = p
+		if r-l > result {
+			result = r - l
+		}
+
+		if p < l {
+			l = p
+		}
+	}
+	return result
+}
