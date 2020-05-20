12fc85ec8dc6d42cc95b71fa51298d7e25c082c9
irlndts <kleto4kin@gmail.com>
2020-05-20 18:11:03 +0300

    Group anagrams

---
+++ 49-group-anagrams.go
@@ -0,0 +1,35 @@
+package leetcode
+
+import (
+	"sort"
+	"strings"
+)
+
+// https://leetcode.com/problems/group-anagrams/
+
+func groupAnagrams(strs []string) [][]string {
+	if len(strs) == 0 {
+		return nil
+	}
+
+	var result [][]string
+
+	var i int
+	m := make(map[string]int)
+	for _, str := range strs {
+		ls := strings.Split(str, "")
+		sort.Strings(ls)
+		key := strings.Join(ls, "")
+		v, ok := m[key]
+		if !ok {
+			m[key] = i
+			result = append(result, []string{str})
+			i++
+			continue
+		}
+
+		result[v] = append(result[v], str)
+	}
+
+	return result
+}
