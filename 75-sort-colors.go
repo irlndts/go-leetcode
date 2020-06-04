978ddae106dd57abf2dd6278b8969349c730d437
irlndts <kleto4kin@gmail.com>
2020-06-04 17:51:29 +0300

    Sort Colors

---
+++ 75-sort-colors.go
@@ -0,0 +1,23 @@
+package leetcode
+
+// https://leetcode.com/problems/sort-colors/
+
+func sortColors(nums []int) {
+	m := map[int]int{
+		0: 0,
+		1: 0,
+		2: 0,
+	}
+
+	for _, n := range nums {
+		m[n]++
+	}
+
+	c := 0
+	for i := range []int{0, 1, 2} {
+		for j := 0; j < m[i]; j++ {
+			nums[j+c] = i
+		}
+		c += m[i]
+	}
+}
