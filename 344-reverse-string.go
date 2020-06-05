50c8b940110998938d438c05009cc0171c921dbf
irlndts <kleto4kin@gmail.com>
2020-06-05 16:11:17 +0300

    Reverse string

---
+++ 344-reverse-string.go
@@ -0,0 +1,12 @@
+package leetcode
+
+// https://leetcode.com/problems/reverse-string/
+
+func reverseString(s []byte) {
+	l, r := 0, len(s)-1
+	for l < r {
+		s[l], s[r] = s[r], s[l]
+		l++
+		r--
+	}
+}
