86fe05edd710f994c6caa51229f0be98c460620d
irlndts <kleto4kin@gmail.com>
2020-06-05 16:08:58 +0300

    Hamming distance

---
+++ 461-hamming-distance.go
@@ -0,0 +1,42 @@
+package leetcode
+
+import (
+	"fmt"
+	"strings"
+)
+
+// https://leetcode.com/problems/hamming-distance/
+
+func hammingDistance(x int, y int) int {
+	a := fmt.Sprintf("%b", x)
+	b := fmt.Sprintf("%b", y)
+
+	a, b = foo(a, b)
+
+	var result int
+	for i := range a {
+		if a[i] != b[i] {
+			result++
+		}
+	}
+
+	return result
+}
+
+func foo(a, b string) (string, string) {
+
+	l := len(a)
+	if len(b) > len(a) {
+		l = len(b)
+	}
+
+	if len(a) < l {
+		a = strings.Repeat("0", l-len(a)) + a
+	}
+
+	if len(b) < l {
+		b = strings.Repeat("0", l-len(b)) + b
+	}
+
+	return a, b
+}
