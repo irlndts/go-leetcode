14cc0c123004e461fa22fa6068bb5985d2cecb4d
irlndts <kleto4kin@gmail.com>
2020-06-06 18:16:56 +0300

    Delete node in a linked list

---
+++ 237-delete-node-in-a-linked-list.go
@@ -0,0 +1,15 @@
+package leetcode
+
+// https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteNode(node *ListNode) {
+	node.Val = node.Next.Val
+	node.Next = node.Next.Next
+}
