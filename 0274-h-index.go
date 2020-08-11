cc5ccf9f73a4a58d6506abf121435880c75ce15f
irlndts <kleto4kin@gmail.com>
2020-08-11 10:22:41 +0300

    274 H-Index

---
+++ 0274-h-index.go
@@ -0,0 +1,13 @@
+package leetcode
+
+import "sort"
+
+func hIndex(citations []int) int {
+	sort.Ints(citations)
+	for i := 0; i < len(citations); i++ {
+		if citations[i] >= len(citations)-i {
+			return len(citations) - i
+		}
+	}
+	return 0
+}
