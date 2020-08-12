9fc2daa89204f396d442936199f25f005b132618
irlndts <kleto4kin@gmail.com>
2020-08-12 18:16:55 +0300

    63 Unique Paths II

---
+++ 0063-unique-paths-ii.go
@@ -0,0 +1,41 @@
+package leetcode
+
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	if len(obstacleGrid) == 1 && len(obstacleGrid[0]) == 1 &&
+		obstacleGrid[0][0] != 1 {
+		return 1
+	}
+
+	if obstacleGrid[0][0] == 1 {
+		return 0
+	}
+
+	dp := make([][]int, len(obstacleGrid))
+	for i := range dp {
+		dp[i] = make([]int, len(obstacleGrid[0]))
+	}
+
+	for i := 1; i < len(dp); i++ {
+		if obstacleGrid[i][0] == 1 {
+			break
+		}
+		dp[i][0] = 1
+	}
+
+	for i := 1; i < len(dp[0]); i++ {
+		if obstacleGrid[0][i] == 1 {
+			break
+		}
+		dp[0][i] = 1
+	}
+
+	for i := 1; i < len(dp); i++ {
+		for j := 1; j < len(dp[0]); j++ {
+			if obstacleGrid[i][j] != 1 {
+				dp[i][j] = dp[i-1][j] + dp[i][j-1]
+			}
+		}
+	}
+
+	return dp[len(dp)-1][len(dp[0])-1]
+}
