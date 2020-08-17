5721dac372d7ffea72af6cb84e8dd0cb6961d883
irlndts <kleto4kin@gmail.com>
2020-08-17 10:56:05 +0300

    1103 Distribute candies to people

---
+++ 1103-distribute-candies-to-people.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func distributeCandies(candies int, numPeople int) []int {
+	result := make([]int, numPeople)
+	index := 0
+	current := 1
+	for candies-current >= 0 {
+		result[index] += current
+		candies -= current
+		current++
+
+		index++
+		if index == numPeople {
+			index = 0
+		}
+	}
+	result[index] += candies
+	return result
+}
