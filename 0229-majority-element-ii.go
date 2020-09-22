a8e2080157a8502d1429a5fdd12208365a982aa5
irlndts <kleto4kin@gmail.com>
2020-09-22 10:40:25 +0300

    329 Majority Element II

---
+++ 0229-majority-element-ii.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func majorityElement(nums []int) []int {
+	m := map[int]int{}
+	result := []int{}
+	for _, num := range nums {
+		m[num]++
+		if m[num] == len(nums)/3+1 {
+			result = append(result, num)
+		}
+	}
+	return result
+}
