a4dd46f1081141d6a7ebd96466c70473e270383e
irlndts <kleto4kin@gmail.com>
2021-02-18 15:16:24 +0300

    1732. Find the Highest Altitude

---
+++ 1732-find-the-highest-altitude.go
@@ -0,0 +1,15 @@
+package leetcode
+
+// https://leetcode.com/problems/find-the-highest-altitude/
+
+func largestAltitude(gain []int) int {
+	current, max := 0, 0
+	for _, g := range gain {
+		current += g
+		if current > max {
+			max = current
+		}
+	}
+
+	return max
+}
